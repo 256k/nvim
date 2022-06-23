cb6aad5b4c5538412c99f66059f438cc8500180e
fayek helmi <fhelmi@absolunet.com>
2022-06-23 07:08:07 -0400

    replaced vimscript config with lua

---
+++ lua/settings/options.lua
@@ -0,0 +1,26 @@
+local set = vim.opt
+
+set.number = true
+set.relativenumber = true
+
+set.tabstop=2
+set.softtabstop=2
+set.shiftwidth=2
+set.expandtab=true
+set.smartindent=true
+set.number=true
+set.relativenumber=true
+set.hlsearch=false
+set.hidden=true
+set.swapfile=false
+set.backup=false
+set.undodir="~/.vim/undodir"
+set.undofile=true
+set.incsearch=true
+set.scrolloff=15
+set.wrap=false
+set.smartcase=true
+set.ignorecase=true
+set.mouse=a
+set.splitbelow=true
+set.signcolumn='auto:1-9'
