5395909476c5afbf9b1a37440acd71ecbd1d8b2e
fayek helmi <fhelmi@absolunet.com>
2022-06-23 12:26:14 -0400

    added more mappings

--- lua/settings/mappings.lua
+++ lua/settings/mappings.lua
@@ -7,6 +7,26 @@ local opt = {noremap = true, silent = true}
 -- keymap( [mode], [character], [action], [options]) 
 --
 
+-- reload config files:
 vim.api.nvim_set_keymap("n", "<leader>r", ":luafile %<cr>", {noremap = true, silent = true})
--- vim.api.nvim_set_keymap("n", "<leader>r", ":luafile %<cr>", {noremap = true})
 
+-- move up & down 10 spaces:
+vim.api.nvim_set_keymap("n", "<C-j>", "10j", {noremap = true})
+vim.api.nvim_set_keymap("n", "<C-k>", "10k", {noremap = true})
+
+
+-- splitting window:
+vim.api.nvim_set_keymap("n", "<leader>vs", ":vsplit<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>s", ":split<cr>", {noremap = true})
+
+-- split window navigation (CONTROL + SHIFT + h,j,k,l)
+vim.api.nvim_set_keymap("n", "<C-H>", "<C-w>h", {noremap = true})
+vim.api.nvim_set_keymap("n", "<C-J>", "<C-w>j", {noremap = true})
+vim.api.nvim_set_keymap("n", "<C-K>", "<C-w>k", {noremap = true})
+vim.api.nvim_set_keymap("n", "<C-L>", "<C-w>l", {noremap = true})
+
+
+-- buffer moving:
+
+vim.api.nvim_set_keymap("n", "<Tab>", ":bn<cr>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<S-Tab>", ":bp<cr>", {noremap = true})
