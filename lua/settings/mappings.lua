5e0d0ea2a51a11322e8bb5d0bd86c061871acb40
fayek helmi <fhelmi@absolunet.com>
2022-06-23 21:48:44 -0400

    adjusted keymaps and added plugins file

--- lua/settings/mappings.lua
+++ lua/settings/mappings.lua
@@ -8,25 +8,25 @@ local opt = {noremap = true, silent = true}
 --
 
 -- reload config files:
-vim.api.nvim_set_keymap("n", "<leader>r", ":luafile %<cr>", {noremap = true, silent = true})
-
--- move up & down 10 spaces:
-vim.api.nvim_set_keymap("n", "<C-j>", "10j", {noremap = true})
-vim.api.nvim_set_keymap("n", "<C-k>", "10k", {noremap = true})
+keymap("n", "<leader>r", ":luafile %<cr>", opt )
 
 
 -- splitting window:
-vim.api.nvim_set_keymap("n", "<leader>vs", ":vsplit<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<leader>s", ":split<cr>", {noremap = true})
+keymap("n", "<leader>vs", ":vsplit<cr>", opt )
+keymap("n", "<leader>s", ":split<cr>", opt )
 
 -- split window navigation (CONTROL + SHIFT + h,j,k,l)
-vim.api.nvim_set_keymap("n", "<C-H>", "<C-w>h", {noremap = true})
-vim.api.nvim_set_keymap("n", "<C-J>", "<C-w>j", {noremap = true})
-vim.api.nvim_set_keymap("n", "<C-K>", "<C-w>k", {noremap = true})
-vim.api.nvim_set_keymap("n", "<C-L>", "<C-w>l", {noremap = true})
+keymap("n", "<C-H>", "<C-w>h", opt )
+keymap("n", "<C-J>", "<C-w>j", opt )
+keymap("n", "<C-K>", "<C-w>k", opt )
+keymap("n", "<C-L>", "<C-w>l", opt )
 
+
+-- move up & down 10 spaces:
+keymap("n", "<C-j>", "10j", opt )
+keymap("n", "<C-k>", "10k", opt )
 
 -- buffer moving:
 
-vim.api.nvim_set_keymap("n", "<Tab>", ":bn<cr>", {noremap = true})
-vim.api.nvim_set_keymap("n", "<S-Tab>", ":bp<cr>", {noremap = true})
+keymap("n", "<Tab>", ":bn<cr>", opt )
+keymap("n", "<S-Tab>", ":bp<cr>", opt )
---
+++ lua/settings/plugins.lua
@@ -0,0 +1,12 @@
+
+-- " Plug 'airblade/vim-gitgutter'
+-- " Plug 'tpope/vim-surround' " this is the select an object and surround it with brackets or change its surrounding brackets
+-- " Plug 'nvim-lua/plenary.nvim' " required for telescope
+-- " Plug 'nvim-telescope/telescope.nvim' " a fuzzy search plugin for neovim (fzf replacement)
+-- " Plug 'EdenEast/nightfox.nvim' " color scheme
+-- " Plug 'preservim/nerdtree' " tree folder manager
+-- " Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+-- " Plug 'glepnir/indent-guides.nvim' "indentation 
+-- Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+-- Plug 'junegunn/fzf.vim'
+-- call plug#end()
